5ce556ce6d7c4e5958b54e8dc04984a92462cc25
Hrvoje <vhrvoje2@yahoo.com>
2020-12-24 19:42:13 +0100

    Added Project 30

---
+++ Project 30/script.js
@@ -0,0 +1,26 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const inputEl = document.getElementById('msg')
+let text = 'Type something!'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText(){
+    textEl.innerText = text.slice(0, idx)
+    idx++
+    if (idx > text.length){
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('change', () => {
+    speed = 300 / speedEl.value
+})
+
+inputEl.addEventListener('keyup', () => {
+    text = inputEl.value
+})
